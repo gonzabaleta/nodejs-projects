3e1eaa8529d30ae00385e0f10790200e808185eb
gonzabaleta <gonza.zf@gmail.com>
2022-06-13 15:04:04 -0300

    initial commit

---
+++ getDefaultProducts.js
@@ -0,0 +1,20 @@
+const getDefaultProducts = () => [
+  {
+    id: 1,
+    title: "Fideos",
+    price: 80,
+  },
+
+  {
+    id: 2,
+    title: "Arroz",
+    price: 100,
+  },
+  {
+    id: 3,
+    title: "Queso",
+    price: 300,
+  },
+];
+
+module.exports = { getDefaultProducts };
--- server.js
+++ server.js
@@ -1,23 +1,84 @@
 const express = require("express");
 
-const contenedor = require("./container");
+const { Router } = express;
 
 const app = express();
+const router = Router();
 
-app.get("/products", async (req, res) => {
-  const products = await contenedor.getAll();
-  res.send(products);
-});
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/productoRandom", async (req, res) => {
-  const products = await contenedor.getAll();
-  const min = 0;
-  const max = products.length - 1;
-  const index = Math.floor(Math.random() * (max - min + 1) + min);
-  const selectedProduct = products[index];
-  res.send(selectedProduct);
-});
+const { getDefaultProducts } = require("./getDefaultProducts.js");
 
-const PORT = process.env.port || 8080;
+const products = getDefaultProducts();
 
-const server = app.listen(PORT, () => console.log("escuchando puerto " + 8080));
+let prodID = products[products.length - 1].id;
+
+router
+  .route("/productos")
+
+  // GET: Devuelve todos los productos
+  .get((req, res) => res.json(products))
+
+  // POST: Agrega un producto
+  .post((req, res) => {
+    const data = req.body;
+    const id = ++prodID;
+
+    const producto = { id, ...data };
+    products.push(producto);
+
+    res.json(producto);
+  });
+
+// Middleware para buscar el producto en las rutas con params
+const findProduct = (req, res, next) => {
+  const { id } = req.params;
+  const productID = products.findIndex((prod) => prod.id === +id);
+
+  if (productID === -1) {
+    res.json({ error: "Producto no encontrado" });
+  } else {
+    res.locals.index = productID;
+    next();
+  }
+};
+
+router
+  .route("/productos/:id")
+  .all(findProduct)
+
+  // GET: Devuelve un producto por ID
+  .get((req, res) => {
+    const { index } = res.locals;
+
+    res.json(products[index]);
+  })
+
+  // PUT: Actualiza un producto por ID
+  .put((req, res) => {
+    const { index } = res.locals;
+    const data = req.body;
+
+    products[index] = { ...products[index], ...data };
+
+    res.json(products[index]);
+  })
+
+  // DELETE: Elimina un producto por ID
+  .delete((req, res) => {
+    const { index } = res.locals;
+    const product = products[index];
+    products.splice(index, 1);
+
+    res.json(product);
+  });
+
+app.use("/api", router);
+
+const PORT = 3000;
+
+const server = app.listen(PORT, () => console.log(`Escuchando puerto ${PORT}`));
+
+server.on("error", (err) => res.send(`Error de servidor! ${err}`));
