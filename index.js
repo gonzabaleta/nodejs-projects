3579e75ff3197bfbca0aaffc79474dd80a5d844b
gonzabaleta <gonza.zf@gmail.com>
2022-09-03 21:24:01 -0300

    clase 30

--- index.js
+++ index.js
@@ -9,151 +9,171 @@ const passport = require("passport");
 const { Strategy } = require("passport-local");
 const parseArgs = require("minimist");
 const { fork } = require("child_process");
+const cpus = require("os").cpus().length;
+const cluster = require("cluster");
 
-// Config dotenv && dirname
-dotenv.config();
-
-// config EXPRESS
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Configure Session
-app.use(
-  session({
-    secret: "SECRET",
-    resave: true,
-    saveUninitialized: true,
-    cookie: { maxAge: 10 * 60 * 1000 },
-  })
-);
-
-// Set up Template Engines
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", __dirname + "/views");
-
-// Connect MONGO
-const { MONGO_ATLAS_URL } = process.env;
-mongoose.connect(MONGO_ATLAS_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-/**********
- *
- * PASSPORT
- *
- *********/
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Password validation helper:
-const validatePassword = (user, password) =>
-  bcrypt.compareSync(password, user.password);
-
-// Create local strategy
-passport.use(
-  new Strategy(
-    { usernameField: "email", passwordField: "password" },
-    async (email, password, done) => {
-      const user = await model.findOne({ email });
-
-      if (!user) {
-        console.log("User not found");
-        return done(null, false);
-      }
-
-      if (!validatePassword(user, password)) {
-        console.log("invalid password");
-        return done(null, false);
-      }
-      return done(null, user);
-    }
-  )
-);
-
-passport.serializeUser((user, done) => done(null, user._id));
-passport.deserializeUser((id, done) => model.findById(id, done));
-
-// ROUTES
-app.get("/login", (req, res) => {
-  // If user is authenticated, redirect to home, else render login screen
-  if (req.isAuthenticated()) {
-    res.redirect("/");
-  } else {
-    res.render("login");
-  }
-});
-
-// If user is authenticated, redirect to home, else reject login
-app.post("/login", passport.authenticate("local"), (req, res) => {
-  if (req.isAuthenticated()) {
-    res.redirect("/");
-  } else {
-    res.send("CREDENCIALES INVALIDAS");
-  }
-});
-
-// If user is authenticated, render home, else redirect to login
-app.get("/", (req, res) => {
-  if (req.isAuthenticated()) {
-    res.render("home", { email: req.user.email });
-  } else {
-    res.redirect("/login");
-  }
-});
-
-app.get("/register", (req, res) => {
-  if (req.isAuthenticated()) {
-    res.redirect("/");
-  } else {
-    res.render("register");
-  }
-});
-
-app.post("/register", async (req, res) => {
-  const { email, password } = req.body;
-  const hash = await bcrypt.hash(password, 10);
-
-  const user = { email, password: hash };
-  const userSaveModel = new model(user);
-  await userSaveModel.save();
-
-  res.redirect("/login");
-});
-
-app.get("/info", (req, res) => {
-  const response = {
-    Args: parseArgs(process.argv.slice(2)),
-    SO: process.platform,
-    Version: process.version,
-    rss: process.memoryUsage(),
-    Path: process.title,
-    ID: process.id,
-    Directory: process.cwd(),
-  };
-
-  res.send(JSON.stringify(response));
-});
-
-app.get("/api/random", (req, res) => {
-  const amount = req.query.cant || 100000000;
-
-  const child = fork("./generateNumbers");
-  child.send(amount);
-  child.on("message", (numbers) => {
-    res.json(numbers);
-  });
-});
-
-// Configure PORT
+// Parse args
 const args = parseArgs(process.argv.slice(2));
-const PORT = args.p || 8080;
 
-app.listen(PORT, (err) => {
-  if (err) throw new Error(`Error de servidor ${err}`);
-  console.log("Escuchando puerto ", PORT);
-});
+// Configure CLUSTER if arg --cluster is passed
+if (args.cluster === true && cluster.isMaster) {
+  console.log(`El proceso master ${process.pid} está corriendo.`);
+
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} termiado.`);
+  });
+} else {
+  // Config dotenv && dirname
+  dotenv.config();
+
+  // config EXPRESS
+  const app = express();
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  // Configure Session
+  app.use(
+    session({
+      secret: "SECRET",
+      resave: true,
+      saveUninitialized: true,
+      cookie: { maxAge: 10 * 60 * 1000 },
+    })
+  );
+
+  // Set up Template Engines
+  app.engine("handlebars", engine());
+  app.set("view engine", "handlebars");
+  app.set("views", __dirname + "/views");
+
+  // Connect MONGO
+  const { MONGO_ATLAS_URL } = process.env;
+  mongoose.connect(MONGO_ATLAS_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  /**********
+   *
+   * PASSPORT
+   *
+   *********/
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // Password validation helper:
+  const validatePassword = (user, password) =>
+    bcrypt.compareSync(password, user.password);
+
+  // Create local strategy
+  passport.use(
+    new Strategy(
+      { usernameField: "email", passwordField: "password" },
+      async (email, password, done) => {
+        const user = await model.findOne({ email });
+
+        if (!user) {
+          console.log("User not found");
+          return done(null, false);
+        }
+
+        if (!validatePassword(user, password)) {
+          console.log("invalid password");
+          return done(null, false);
+        }
+        return done(null, user);
+      }
+    )
+  );
+
+  passport.serializeUser((user, done) => done(null, user._id));
+  passport.deserializeUser((id, done) => model.findById(id, done));
+
+  // ROUTES
+  app.get("/login", (req, res) => {
+    // If user is authenticated, redirect to home, else render login screen
+    if (req.isAuthenticated()) {
+      res.redirect("/");
+    } else {
+      res.render("login");
+    }
+  });
+
+  // If user is authenticated, redirect to home, else reject login
+  app.post("/login", passport.authenticate("local"), (req, res) => {
+    if (req.isAuthenticated()) {
+      res.redirect("/");
+    } else {
+      res.send("CREDENCIALES INVALIDAS");
+    }
+  });
+
+  // If user is authenticated, render home, else redirect to login
+  app.get("/", (req, res) => {
+    if (req.isAuthenticated()) {
+      res.render("home", { email: req.user.email });
+    } else {
+      res.redirect("/login");
+    }
+  });
+
+  app.get("/register", (req, res) => {
+    if (req.isAuthenticated()) {
+      res.redirect("/");
+    } else {
+      res.render("register");
+    }
+  });
+
+  app.post("/register", async (req, res) => {
+    const { email, password } = req.body;
+    const hash = await bcrypt.hash(password, 10);
+
+    const user = { email, password: hash };
+    const userSaveModel = new model(user);
+    await userSaveModel.save();
+
+    res.redirect("/login");
+  });
+
+  app.get("/info", (req, res) => {
+    const response = {
+      Args: args,
+      SO: process.platform,
+      Version: process.version,
+      rss: process.memoryUsage(),
+      Path: process.title,
+      ID: process.id,
+      Directory: process.cwd(),
+      CPUs: cpus,
+    };
+
+    console.log(`Recibido en ${process.pid}`);
+
+    res.send(JSON.stringify(response));
+  });
+
+  app.get("/api/random", (req, res) => {
+    const amount = req.query.cant || 100000000;
+
+    const child = fork("./generateNumbers");
+    child.send(amount);
+    child.on("message", (numbers) => {
+      res.json(numbers);
+    });
+  });
+
+  // Configure PORT
+  const PORT = args.p || 8080;
+
+  app.listen(PORT, (err) => {
+    if (err) throw new Error(`Error de servidor ${err}`);
+    console.log("Escuchando puerto ", PORT);
+  });
+}
